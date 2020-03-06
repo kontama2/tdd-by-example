87464561f7d97279f267208085f22368de4a3e73
kontama2 <kontama2p@gmail.com>
2020-03-06 22:55:13 +0900

    Read chapter-13. (I think this chapter is difficult to understand contents!).

---
+++ src/money/Sum.java
@@ -0,0 +1,17 @@
+package money;
+
+public class Sum implements Expression {
+
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+    public Money augend;
+    public Money addend;
+	public Money reduce(String to) {
+        int amount = augend.amount + addend.amount;
+		return new Money(amount, to);
+	}
+
+}
